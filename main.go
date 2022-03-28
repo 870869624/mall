8f8b13b9fa0f8dc9f17ec4652db9efcd3b063e71
peng.deng <peng.deng@coloseo.cn>
2022-03-28 23:37:04 +0800

    message

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
   	"gorm.io/driver/mysql"
   	"gorm.io/gorm"
+	"crypto/sha256"
 )
 
 // 基础user
@@ -19,6 +20,17 @@ type RegisterUser struct {
 	User
 	Name string
 }
+
+type PasswordChange struct{
+	Id int
+	Password string
+}
+type ChangeName struct{
+	ID int
+	User
+	Name string
+}
+
 
 func pin (c *gin.Context){
 		c.JSON(200, gin.H{
@@ -36,8 +48,10 @@ func register(c *gin.Context) {
 	if err != nil {
 		panic("db connect error")
 	}
-
-	sql := "insert into users(name, username, password, gender, headpicture, age) values ('" + user.Name + "', '" + user.Username + "', '" + user.Password +"', 0, '', 0)";
+	h := sha256.New()
+	h.Write([]byte(user.Password))
+	cryptoPassword := fmt.Sprintf("%x", h.Sum(nil))
+	sql := "insert into users(name, username, password, gender, headpicture, age) values ('" + user.Name + "', '" + user.Username + "', '" + cryptoPassword +"', 0, '', 0)";
 	e := db.Exec(sql);
 	if e == nil {
 		c.JSON(400, gin.H{
@@ -61,61 +75,95 @@ func register(c *gin.Context) {
 func login(c *gin.Context){
 	// 1 从请求体(json)中取出传过来的用户名和密码
 	var user User
+	var check User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	
-
-
-	
-	
-
-	var check User
 	dsn := "root:123456@tcp(127.0.0.1:3306)/mall?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil{
 		panic("db connect error")
 	}
-	db.Raw(
-		"select username, password from users where username = ? and password = ?", 
-		user.Username, 
-		user.Password
-	).Scan(&check)
+	h := sha256.New()
+	h.Write([]byte(user.Password))
+	cryptoPassword := fmt.Sprintf("%x", h.Sum(nil))
+	db.Raw("select username, password from users where username = ? and password = ?", user.Username, cryptoPassword).Scan(&check)
 	fmt.Println("result", check.Username, check.Password);
-	// if check == nil {
-	// 	c.JSON(400, gin.H{
-	// 		"message": "登录失败",
-	// 	})
-	// } else {
+	if check.Username == "" {
+		c.JSON(400, gin.H{
+			"message": "登录失败",
+		})
+	} else {
 		c.JSON(200, gin.H{
 			"message": "登录成功",
 			"username": check.Username,
 			"password": check.Password,
 		})
-	// }
+	}
 }
-// func getting(c *gin.Context){
-// 	var user1 User
-// 	if err := c.ShouldBindJSON(&user1); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	dsn := "root:123456@tcp(127.0.0.1:3306)/mall?charset=utf8mb4&parseTime=True&loc=Local"
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-// 	if err != nil{
-// 		panic("db connect error")
-// 	}
-	
-// }
+func changePassword(c *gin.Context){
+	var data PasswordChange
+	// 从请求体里获取用户ID和密码
+	err := c.ShouldBindJSON(&data)
+	if err != nil{
+		c.JSON(400, gin.H{"message": "参数错误"})
+		return
+	}
+	fmt.Println("----", data.Id, data.Password)
+	if data.Id == 0 || data.Password == ""{ 
+		c.JSON(400, gin.H{"message": "参数不全"})
+	}
+	dsn := "root:123456@tcp(127.0.0.1:3306)/mall?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil{
+		panic("db connect error")
+	}
+	var record PasswordChange
+	db.Raw("select password, id from users where id = ?", data.Id).Scan(&record)
+	fmt.Println("record:", record.Id, record.Password)
+	if record.Id == 0{
+		c.JSON(400, gin.H{"message": "没有找到该用户"})
+	}
+	// 加密data.Password，使用sha256
+	// 构造加密因子
+	h := sha256.New()
+	h.Write([]byte(data.Password))
+	cryptoPassword := fmt.Sprintf("%x", h.Sum(nil))
+	result := db.Exec("update users set password = ? where id = ?", cryptoPassword, data.Id)
+	if result.Error != nil {
+		c.JSON(400, gin.H{"message": "操作失败"})
+		return;
+	}
+		c.JSON(200, gin.H{"message": "修改成功"})
+}
+func NameChange(c *gin.Context){
+	var NewName ChangeName
+	if err := c.ShouldBindJSON(&NewName); err != nil{
+		c.JSON(400, gin.H{"message": "参数错误"})
+		return
+	}
+	fmt.Println(NewName.ID, NewName.Username, NewName.Password, NewName.Name)
+	if NewName.ID == 0 || NewName.Name == ""{
+		c.JSON(400, gin.H{"message": "参数错误"})
+		return 
+	}
+	dsn := "root:123456@tcp(127.0.0.1:3306)/mall?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil{
+		panic("db connect error")
+	}
+	db.Exec("update users set name = ? where id = ?", NewName.Name, NewName.ID)
+}
 
-func main() {
 
+func main(){
 	r := gin.Default()
 	r.GET("/ping", pin)
-
-	r.POST("/register", register)
-	r.POST("/login", login)
-	r.GET("/someGet", getting)
+	r.POST("user/register", register)
+	r.POST("user/login", login)
+	r.POST("user/nameChange", NameChange)
+	r.POST("/password/reset", changePassword)
+	// r.post("/addtocart", addtocart)
 	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
